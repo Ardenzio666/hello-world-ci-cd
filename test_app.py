124ee85b71c754c75dc8824ff44360546be0ed1b
Tiziano Pacifico <gourmetmolise@gmail.com>
2025-11-02 12:40:21 +0100

    feat: testing pipeline

--- test_app.py
+++ test_app.py
@@ -5,7 +5,7 @@ from app import hello_world
 
 class TestApp(unittest.TestCase):
     def test_hello_world(self):
-        self.assertEquals(hello_world(), "Hello, World!")
+        self.assertEqual(hello_world(), "Hello, world!!")
 
 if __name__ == "__main__":
     unittest.main()
